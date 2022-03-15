d4e3d8666ba771c607fbb69441512ea9cefb6f78
Hector Bai <hbai@greendotcorp.com>
2022-03-15 13:15:58 +0800

    initial commint

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Hector Bai on 2022/3/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
